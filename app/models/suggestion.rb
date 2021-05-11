075f04ad508d4d6854c0ffdb3100ae733ed11bf5
Jenner Thomas <jthomas@jthomas-mbp.attlocal.net>
2021-05-11 18:22:09 -0400

    styling changes

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -4,6 +4,8 @@ class Suggestion < ApplicationRecord
   validates :user, length: {minimum: 2, maximum: 20}
   # validates :body, format: { with: /\A[a-zA-Z]+\z/,
   #   message: "Only Letters Please" }
+  validates :upvotes, numericality: { less_than: 50}
+  validates :downvotes, numericality: { less_than: 50}
 
   #
 end
